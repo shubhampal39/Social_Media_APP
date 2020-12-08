49642e1d7c1c4aa3a9ec24efc57d0951459135eb
shubhampal39 <shubhampal39@gmail.com>
2020-12-08 18:42:15 +0530

    added public and views folder

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const express=require('express');
 const exphbs=require('express-handlebars');
 const mongoose=require('mongoose');
-
-//conne
 const keys=require('./config/keys');
+//conne
+// const keys=require('./config/keys');
 const app=express();
 
 app.engine('handlebars',exphbs({
@@ -13,15 +13,17 @@ app.engine('handlebars',exphbs({
 
 app.set('view engine','handlebars');
 app.use(express.static('public'));
+//const port=4400;
 
-mongoose.connect(keys.MongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect(keys.MongoURI
-	//,{useNewUrlParser: true}
+//mongoose.connect(keys.MongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(keys.MongoURI,
+	{useNewUrlParser: true, useUnifiedTopology: true}
 	).then(()=>{
 	console.log("Connected to remote DataBase---");
 }).catch((err)=>{
 	console.log(err)
 })
+
 
 
 const port=process.env.PORT||4000;
@@ -31,12 +33,12 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/about',(req,res)=>{
-	res.render('about');
+	res.send('about');
 });
 
-app.get('/',(req,res)=>{
-	res.send('welcome to social media app');
-});
+// app.get('/',(req,res)=>{
+// 	res.send('welcome to social media app');
+// });
 
 
 
