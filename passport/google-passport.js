4f82b4033129125d5ed1df79fa4ca92c438fc23d
shubhampal39 <shubhampal39@gmail.com>
2021-01-17 19:34:45 +0530

    Added data into mongoose database

--- passport/google-passport.js
+++ passport/google-passport.js
@@ -13,8 +13,6 @@ passport.serializeUser(function(user, done) {
     });
   });
 
-
-
 passport.use(new GoogleStrategy({
     clientID:keys.GoogleClientID,
     clientSecret: keys.GoogleClientSecret,
@@ -23,8 +21,31 @@ passport.use(new GoogleStrategy({
     prooxy:true
   },
   (accessToken, refreshToken, profile,cb)=>{
-      console.log("Profile::::::::::::",profile);
-  }
+      // console.log("Profile::::::::::::",profile);
+      //storing to database
+     User.findOne({
+       google:profile.id//goggle is in user.js db
+     }).then((user)=>{
+       if(user){
+         done(null,user);
+       }else{
+         const newUser={
+           google:profile.id,
+           fullname:profile.displayName,
+           firstname:profile.name.givenName,
+           lastname:profile.name.familyName,
+           image:profile.photos[0].value
+         }
+         new User(newUser).save()
+         .then((user)=>{
+           done(null,user);
+         })
+       }
+     })
+  
+    }
+
+
 //   function(request, accessToken, refreshToken, profile, done) {
 //     User.findOrCreate({ googleId: profile.id }, function (err, user) {
 //       return done(err, user);
