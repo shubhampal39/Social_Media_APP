5ba467d27bfce32a41fc8272da76725a4c58445e
shubhampal39 <shubhampal39@gmail.com>
2021-01-19 02:56:32 +0530

    Added google authentication for production and development separately

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports={
+    MongoURI:'mongodb+srv://socialmediaapp:socialmediaapp@onlinedatingapp.2pwza.mongodb.net/user?retryWrites=true&w=majority',
+    GoogleClientID:'1085086803497-sr5qrijrl71c7vqgi16jvkplc8jc12ds.apps.googleusercontent.com',
+    GoogleClientSecret:'POu1kugcow2-5FSDIrA2Yc9Z'
+}
