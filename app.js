d8f5c69811bff547580a8307b310a18d35a13666
shubhampal39 <shubhampal39@gmail.com>
2021-01-19 03:29:26 +0530

    deployment

--- app.js
+++ app.js
@@ -14,38 +14,38 @@ require('./passport/google-passport');
 // initialize application
 const app = express();
 // Express config
- app.use(cookieParser());
- app.use(bodyParser.urlencoded({
-     extended: false
- }));
- app.use(bodyParser.json());
- app.use(session({
-     secret: 'keyboard cat',
-     resave: true,
-     saveUninitialized: true
- }));
- app.use(passport.initialize());
- app.use(passport.session());
- // set global vars for user
- app.use((req, res, next) => {
-     res.locals.user = req.user || null;
-     next();
- });
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(bodyParser.json());
+app.use(session({
+	secret: 'keyboard cat',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// set global vars for user
+app.use((req, res, next) => {
+	res.locals.user = req.user || null;
+	next();
+});
 // setup template engine
 // app.engine('handlebars', exphbs({
 //     defaultLayout: 'main'
 // }));
 
 app.engine(
-	  "handlebars",
-	  exphbs({
+	"handlebars",
+	exphbs({
 		defaultLayout: "main",
 		runtimeOptions: {
-		  allowProtoPropertiesByDefault: true,
-		  allowProtoMethodsByDefault: true,
+			allowProtoPropertiesByDefault: true,
+			allowProtoMethodsByDefault: true,
 		},
-	  })
-	);
+	})
+);
 
 app.set('view engine', 'handlebars');
 // setup static file to serve css, javascript and images
@@ -53,51 +53,51 @@ app.use(express.static('public'));
 // connect to remote database
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.MongoURI, {
-    useNewUrlParser: true
+	useNewUrlParser: true
 })
-.then(() => {
-    console.log('Connected to Remote Database....');
-}).catch((err) => {
-    console.log(err);
-});
+	.then(() => {
+		console.log('Connected to Remote Database....');
+	}).catch((err) => {
+		console.log(err);
+	});
 // set environment variable for port
 const port = process.env.PORT || 3000;
 // Handle routes
 app.get('/', (req, res) => {
-    res.render('home');
+	res.render('home');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+	res.render('about');
 });
 // GOOGLE AUTH ROUTE
 app.get('/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    }));
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	}));
 
 app.get('/auth/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/'
-    }),
-    (req, res) => {
-        // Successful authentication, redirect home.
-        res.redirect('/profile');
-    });
+	passport.authenticate('google', {
+		failureRedirect: '/'
+	}),
+	(req, res) => {
+		// Successful authentication, redirect home.
+		res.redirect('/profile');
+	});
 app.get('/profile', (req, res) => {
-    User.findById({_id: req.user._id})
-    .then((user) => {
-        res.render('profile', {
-            user:user
-        });
-    })
+	User.findById({ _id: req.user._id })
+		.then((user) => {
+			res.render('profile', {
+				user: user
+			});
+		})
 });
 
-app.get('/logout',(req,res)=>{
+app.get('/logout', (req, res) => {
 	req.logout();
 	res.redirect('/')
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
